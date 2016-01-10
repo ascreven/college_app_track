09f07d09f79f8bfada08784c6a997290452135c9
Alexandra <akscreven@gmail.com>
2016-01-10 16:40:49 -0500

    college list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var url = "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=z5IdkiIm0tmyTnGPTXjFsbzrID3gWbjpMcW1Jz2b&school.degrees_awarded.predominant=3,4&school.main_campus=1&school.online_only=0&school.operating=1&_fields=id,school.name,school.city,school.state,school.zip,school.school_url,school.price_calculator_url,2013.student.size&_sort=size:desc"
+    var url = ""
     $.ajax({
       url: url,
       type: "get",
