08e04cb164a04301f3577b7cee993565f0e8a7ea
Alexandra <akscreven@gmail.com>
2016-01-13 20:22:07 -0500

    added pictures of schools

--- app/db/seeds.js
+++ app/db/seeds.js
@@ -9,13 +9,14 @@ SchoolModel.remove({}, function(err){
 
 
 var schoolsData = [
-  {"name":"Northern Arizona University"},
-  {"name":"University of Arizona","url":"http://www.arizona.edu/"},
-
+  {"name":"Northern Arizona University","photo":"https://upload.wikimedia.org/wikipedia/en/a/a0/NAU_Seal.png","url":"nau.edu/"},
+  {"name":"University of Arizona","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/4/43/University_of_Arizona_Seal.svg/1059px-University_of_Arizona_Seal.svg.png","url":"http://www.arizona.edu/"},
+  {"name":"University of Michigan","photo":"https://upload.wikimedia.org/wikipedia/en/b/b8/Umichigan_color_seal.png","url":"https://www.umich.edu/"},
+  {"name":"Arizona State University","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Arizona_State_University_seal.svg/1024px-Arizona_State_University_seal.svg.png","url":"http://www.asu.edu/"}
 ]
 
 //loop thru schoolsData
-for(var i = 0; i < schoolsData.length; i++){
+for(var i = 0; i <= schoolsData.length; i++){
   var schoolInfo = schoolsData[i]
   console.log("si", schoolInfo)
   var school = new SchoolModel(schoolInfo)
--- app/models/school.js
+++ app/models/school.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var SchoolSchema = new Schema({
   name: String,
+  photo: String,
   url: String
 });
 module.exports = mongoose.model('School', SchoolSchema);
