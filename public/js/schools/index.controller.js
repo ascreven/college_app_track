8cafadd3813ee9d92220f054f87041af5f438a9f
Alexandra <akscreven@gmail.com>
2016-01-14 23:59:05 -0500

    switching

--- public/js/schools/index.controller.js
+++ public/js/schools/index.controller.js
@@ -12,12 +12,8 @@
   function SchoolIndexControllerFunction(SchoolFactory, $scope){
 
     // $scope.schools = SchoolFactory.query();
-    $scope.schools = [
-      {"name":"Northern Arizona University","photo":"https://upload.wikimedia.org/wikipedia/en/a/a0/NAU_Seal.png","url":"nau.edu/"},
-      {"name":"University of Arizona","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/4/43/University_of_Arizona_Seal.svg/1059px-University_of_Arizona_Seal.svg.png","url":"http://www.arizona.edu/","date": '1/26/2016'},
-      {"name":"University of Michigan","photo":"https://upload.wikimedia.org/wikipedia/en/b/b8/Umichigan_color_seal.png","url":"https://www.umich.edu/","date": '1/27/2016'},
-      {"name":"Arizona State University","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Arizona_State_University_seal.svg/1024px-Arizona_State_University_seal.svg.png","url":"http://www.asu.edu/","date": '1/28/2016'}
-]
+    $scope.schools = SchoolFactory.schools
+    console.log($scope.schools)
 
 $scope.addSchool = function(){
   if(!$scope.name || $scope.name === '' || !$scope.photo || $scope.photo === "" || !$scope.url || $scope.url === '') { return; }
@@ -25,6 +21,7 @@ $scope.addSchool = function(){
   $scope.name= '';
   $scope.photo = "";
   $scope.url = "";
+
 };
 $scope.addDate = function(school) {
   $scope.school = this.school
