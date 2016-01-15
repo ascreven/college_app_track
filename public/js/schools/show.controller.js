5827e077f3622eb7fd8fb2c7198339b8f793b6ac
Alexandra <akscreven@gmail.com>
2016-01-14 19:59:28 -0500

    followed tutorial to add user input

--- public/js/schools/show.controller.js
+++ public/js/schools/show.controller.js
@@ -7,12 +7,24 @@
     'SchoolFactory',
     '$stateParams',
     '$scope',
+    '$state',
     SchoolShowControllerFunction
   ]);
 
-  function SchoolShowControllerFunction(SchoolFactory, $stateParams, $scope){
-    this.school = SchoolFactory.get({id: $stateParams.id});
-    console.log($scope)
+  function SchoolShowControllerFunction(SchoolFactory, $stateParams, $scope, $state){
+    // this.school = SchoolFactory.get({id: $stateParams.id});
+    $scope.school = SchoolFactory.get({id: $stateParams.id})
+    console.log($scope.school)
+    $scope.dates = [
+      {date: 'post 1', upvotes: 5},
+      {date: 'post 2', upvotes: 2},
+      {date: 'post 3', upvotes: 15},
+      {date: 'post 4', upvotes: 9},
+      {date: 'post 5', upvotes: 4}
+    ];
+    $scope.addPost = function(){
+  $scope.dates.push({date: 'A new post!', upvotes: 0});
+};
   };
 
 })();
