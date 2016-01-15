e93aeaa8bf73f01782cf76d7d67630d016266235
Alexandra <akscreven@gmail.com>
2016-01-14 21:35:29 -0500

    can click and date shows

--- public/js/schools/index.controller.js
+++ public/js/schools/index.controller.js
@@ -13,7 +13,7 @@
 
     // $scope.schools = SchoolFactory.query();
     $scope.schools = [
-      {"name":"Northern Arizona University","photo":"https://upload.wikimedia.org/wikipedia/en/a/a0/NAU_Seal.png","url":"nau.edu/","date": '1/25/2016'},
+      {"name":"Northern Arizona University","photo":"https://upload.wikimedia.org/wikipedia/en/a/a0/NAU_Seal.png","url":"nau.edu/"},
       {"name":"University of Arizona","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/4/43/University_of_Arizona_Seal.svg/1059px-University_of_Arizona_Seal.svg.png","url":"http://www.arizona.edu/","date": '1/26/2016'},
       {"name":"University of Michigan","photo":"https://upload.wikimedia.org/wikipedia/en/b/b8/Umichigan_color_seal.png","url":"https://www.umich.edu/","date": '1/27/2016'},
       {"name":"Arizona State University","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Arizona_State_University_seal.svg/1024px-Arizona_State_University_seal.svg.png","url":"http://www.asu.edu/","date": '1/28/2016'}
@@ -21,10 +21,19 @@
 
 $scope.addSchool = function(){
   if(!$scope.name || $scope.name === '' || !$scope.photo || $scope.photo === "" || !$scope.url || $scope.url === '') { return; }
-  $scope.schools.push({"name": $scope.name ,"photo": $scope.photo,"url": $scope.url,"date": '1/28/2016'});
+  $scope.schools.push({"name": $scope.name ,"photo": $scope.photo,"url": $scope.url});
   $scope.name= '';
   $scope.photo = "";
   $scope.url = "";
+};
+$scope.addDate = function(school) {
+  $scope.school = this.school
+  $scope.url = this.school.date
+  console.log($scope.url)
+  console.log($scope.school)
+  $('button').click(function(){
+       $(this).text('xxx');
+   })
 };
   }
 })();
